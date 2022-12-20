6ee236973cb5208741ba6a343ff01d47cbd0229a
Ian Derrick <15677606+ianderrick@users.noreply.github.com>
2022-12-20 08:03:13 -0800

    Update pytxtlogcombiner.py

--- pytxtlogcombiner.py
+++ pytxtlogcombiner.py
@@ -2,41 +2,41 @@ import os
 import socket
 import datetime
 
-# get the current date and time
+# Get the current date and time
 now = datetime.datetime.now()
 
-# get the hostname of the computer
+# Get the hostname of the computer
 hostname = socket.gethostname()
 
-# format the date and time to include only the numbers, with no special characters
+# Format the date and time to include only the numbers, with no special characters
 date_string = now.strftime('%Y%m%d')
 
-# create the output file name
+# Create the output file name
 output_file_name = hostname + '_' + date_string + '.txt'
 
-# get the list of all files in the current directory
+# Get the list of all files in the current directory
 files = os.listdir('.')
 
-# open the output file for writing
+# Open the output file for writing
 output_file = open(output_file_name, 'w')
 
-# iterate over the files
+# Iterate over the files
 for file in files:
-    # check if the file is a text file
+# Check if the file is a text file
     if file.endswith('.txt'):
-        # open the file for reading
+# Open the file for reading
         input_file = open(file, 'r')
 
-        # write the file name as a seperator
+# Write the file name as a seperator
         output_file.write('\n\n\n' + '-' * 50 + '\n\n')
         output_file.write('FILE: ' + file + '\n\n')
         output_file.write('-' * 50 + '\n\n')
 
-        # read the contents of the file and write them to the output file
+# Read the contents of the file and write them to the output file
         output_file.write(input_file.read())
 
-        # close the input file
+# Close the input file
         input_file.close()
 
-# close the output file
+# Close the output file
 output_file.close()
